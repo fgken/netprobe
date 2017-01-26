4012cff2a1898730720bdee880b16d7a02e43414
fgken <fgken>
2017-01-27 06:24:41 +0900

    tmp: parse domain

--- parse_domain.c
+++ parse_domain.c
@@ -6,6 +6,21 @@
 #include <debug.h>
 #include <parse_domain.h>
 
+struct domain_header {
+	uint16_t id;
+	uint8_t qr;
+	uint8_t opcode;
+	uint8_t aa;
+	uint8_t tc;
+	uint8_t rd;
+	uint8_t ra;
+	uint8_t rcode;
+	uint16_t qdcount;
+	uint16_t ancount;
+	uint16_t nscount;
+	uint16_t arcount;
+};
+
 void
 handle_domain(const struct base_tuple *tuple, const uint8_t *buf, size_t length)
 {
@@ -13,5 +28,20 @@ handle_domain(const struct base_tuple *tuple, const uint8_t *buf, size_t length)
 	ASSERT(tuple != NULL && buf != NULL);
 
 	MEMDUMP(buf, length);
+
+	/* Header */
+
+	/* Question Section */
+
+	/* Answer Section */
+
+	/* Authority Section */
+
+	/* Additional Section */
+
+	/* EDNS0 */
+
+	/* DNSSEC */
+
 }
 
