a2f4ba1eef7172eaa4ed117877838687f63f1fa6
fgken <fgken>
2017-02-03 02:08:06 +0900

    show tcp window size

--- plugins/tcp_retransmit.c
+++ plugins/tcp_retransmit.c
@@ -16,6 +16,7 @@ struct tcp_session {
 	uint32_t next_sequence;
 	size_t transmit;
 	size_t retransmit;
+	uint32_t window;
 };
 
 static void
@@ -28,11 +29,11 @@ dump_tcp_session(struct tcp_session *session, size_t num)
 	for (i=0; i<num && session[i].hash != 0; i++) {
 		inet_ntop(AF_INET, session[i].tuple.l3.src_ip, src_ip, sizeof(src_ip));
 		inet_ntop(AF_INET, session[i].tuple.l3.dst_ip, dst_ip, sizeof(dst_ip));
-		printf("%s : %u -->> %s : %u - retransmit %zu/%zu\n",
+		printf("%s : %u -->> %s : %u - retransmit %zu/%zu, window = %u\n",
 			src_ip, session[i].tuple.l4.src_port,
 			dst_ip, session[i].tuple.l4.dst_port,
-			session[i].retransmit,
-			session[i].transmit);
+			session[i].retransmit, session[i].transmit,
+			session[i].window);
 	}
 	puts("-------------------------");
 }
@@ -53,6 +54,7 @@ plugin_tcp_retransmit(const struct base_tuple *tuple, const uint8_t *buf, size_t
 	if (tuple->l4.dst_port == 80 || tuple->l4.dst_port == 443) {
 		const struct tcphdr *hdr = (const struct tcphdr *)buf;
 		size_t hdrlen = hdr->th_off << 2;
+		uint16_t window = ntohs(hdr->th_win);
 		uint32_t hash;
 		uint32_t seed = 42;
 		uint8_t key[4+4+2];	/* src_ip, dst_ip, src_port */
@@ -74,12 +76,14 @@ plugin_tcp_retransmit(const struct base_tuple *tuple, const uint8_t *buf, size_t
 					session[i].retransmit++;
 				}
 				session[i].next_sequence = ntohl(hdr->th_seq) + tuple->l3.payload_length - hdrlen;
+				session[i].window = window;
 				break;
 			} else if (session[i].hash == 0) {
 				session[i].hash = hash;
 				memcpy(&session[i].tuple, tuple, sizeof(struct base_tuple));
 				session[i].next_sequence = ntohl(hdr->th_seq) + tuple->l3.payload_length - hdrlen;
 				session[i].transmit++;
+				session[i].window = window;
 				break;
 			}
 		}
